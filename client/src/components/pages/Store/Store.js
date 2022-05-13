d9b327c69ab67b69a83badd62b3c1e5d26836549
johnnydesigns <lovebyfaith@naver.com>
2022-05-13 10:13:18 +0900

    My first commit

---
+++ client/src/components/pages/Store/Store.js
@@ -0,0 +1,9 @@
+import React from "react";
+import '../../App.css';
+import Store_Cards from '../commons/Cards/Store_Cards';
+
+export default function Store() {
+    return <h1 className='store'>
+        <Store_Cards/>
+    </h1>;
+}
