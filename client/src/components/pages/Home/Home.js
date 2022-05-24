6c5ea933177763d6c0f13a51823212f77ece00da
johnny <lovebyfaith@naver.com>
2022-05-24 14:05:03 +0900

    first commit

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,14 +1,18 @@
 import '../../App.css';
 import HeroSection from '../HeroSection/HeroSection';
 import Cards from '../commons/Cards/Cards';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Auth from '../../../hoc/auth';
+import Store_Cards from '../commons/Cards/Store_Cards';
+
 
 function Home() {
+    const [shopdata, setShopdata] = useState([])
     return (
         <>
             <HeroSection />
             <Cards />
+          
         </>
     );
 }
--- client/src/components/pages/commons/Cards/Cards.js
+++ client/src/components/pages/commons/Cards/Cards.js
@@ -5,25 +5,39 @@ import './Cards.css';
 function Cards() {
     return (
         <div className='cards'>
-            <h1>Check out these EPIC Destinations!</h1>
+            <h1>취준생들에게 꼭 필요한 정보 ALLTELIER!</h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
                         <CardItem
-                            src={require('../images/img-9.jpg')}
-                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-                            label='Adventure'
-                            path='/services'
+                            src={require('../images/img-32.jpg')}
+                            text='취준생들과 함께 정보를 공유해보세요.'
+                            label='취준 커뮤니티'
+                            path='/community'
                         />
                         <CardItem
-                            src={require('../images/img-2.jpg')}
-                            text='Travel through the Islands of Bali in a Private Cruise'
-                            label='Luxury'
-                            path='/products'
+                            src={require('../images/img-33.jpg')}
+                            text='취업하고 싶은 분야의 채용정보를 확인하세요.'
+                            label='채용 정보'
+                            path='/recruitment'
                         />
                     </ul>
                     <ul className='cards__items'>
                         <CardItem
+                            src={require('../images/img-31.jpg')}
+                            text='내 주변 착한가게들을 검색해보세요.'
+                            label='착한가게'
+                            path='/store'
+                        />
+                        <CardItem
+                            src={require('../images/img-30.jpg')}
+                            text='와이파이/따릉이/공간을 빌려보세요.'
+                            label='와따공'
+                            path='/wtg'
+                        />
+                    </ul>
+                    <ul className='cards__items'>
+                        {/* <CardItem
                             src={require('../images/img-3.jpg')}
                             text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
                             label='Mystery'
@@ -40,24 +54,24 @@ function Cards() {
                             text='Ride through the Sahara Desert on a guided camel tour'
                             label='Adrenaline'
                             path='/sign-up'
-                        />
+                        /> */}
                     </ul>
                     {/* 추가 */}
                     <ul className='cards__items'>
                         <CardItem
-                            src={require('../images/img-5.jpg')}
+                            src={require('../images/img-34.jpg')}
                             text='취준생들과 함께 정보를 공유해보세요.'
                             label='취준 커뮤니티'
                             path='/community'
                         />
                         <CardItem
-                            src={require('../images/img-6.jpg')}
+                            src={require('../images/img-35.jpg')}
                             text='내 주변 착한가게들을 검색해보세요.'
                             label='착한가게'
                             path='/store'
                         />
                         <CardItem
-                            src={require('../images/img-7.jpg')}
+                            src={require('../images/img-36.jpg')}
                             text='와이파이/따릉이/공간을 빌려보세요.'
                             label='와따공'
                             path='/wtg'
